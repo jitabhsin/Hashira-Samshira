13e6a1644a69bc5bec92e1b557e2401af7d7522d
ABHISHEK SINGH <jitabhsin@gmail.com>
2026-02-21 11:39:14 +0530

    added comments
    Added comments regarding the location of testcase.json and output.

--- Hashira/src/main/java/com/example/Hashira/HashiraApplication.java
+++ Hashira/src/main/java/com/example/Hashira/HashiraApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HashiraApplication {
-
+// The testcase.json file is in resources
+	// output is in command line
 	public static void main(String[] args) {
 		SpringApplication.run(HashiraApplication.class, args);
 	}
